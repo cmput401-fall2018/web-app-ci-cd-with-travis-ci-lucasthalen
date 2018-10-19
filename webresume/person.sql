5ac4aaccd575298d6c661630932f777acb977149
lucasthalen <thalen@ualberta.ca>
2018-10-19 03:41:16 +0000

    Finish assignment 2 and 3
    Modify django settings to allow access
    Create image and txt apis
    Populate person api
    Modify index.html template and style.css for frontend

---
+++ webresume/person.sql
@@ -0,0 +1,10 @@
+INSERT INTO api_person VALUES (
+	1,
+	CURRENT_TIMESTAMP,
+	"Email: thal**@ualberta.ca",
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta, tellus non cursus vestibulum, justo ex laoreet justo, in vulputate erat urna et sem. Curabitur tincidunt accumsan erat quis dignissim. Ut finibus scelerisque eros, ut convallis lectus tristique ac. Nam eu tortor justo. Sed in viverra diam. Praesent sed turpis turpis. Nam sollicitudin massa vel efficitur pharetra. Suspendisse sed egestas nisi, eu dictum odio.",
+	"University of Alberta, BSc.",
+	"Computer/Phone diagnosis and repair; technology sales and consultation",
+	"ACME Ltd.", 
+	"Lucas T"
+);
