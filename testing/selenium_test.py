527e2a68dfc5b138faa2a1d044436524d941cfea
lucasthalen <lucas.thalen@gmail.com>
2018-10-27 20:04:04 -0600

    Fix url in selenium_test.py

--- testing/selenium_test.py
+++ testing/selenium_test.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def test_home():
-    driver = webdriver.Firefox()
-    driver.get("http://127.0.0.1:8000")
+    driver = webdriver.Firefox() # My dev environment has chromium installed, not sure if that works with the Chrome driver.
+    driver.get("http://204.209.76.222:8000")
     e_names = {
         'name':"NAME", 
         'about':"About", 
@@ -29,4 +29,4 @@ def test_home():
         print(resStr)
     driver.quit() # End driver so it doesn't hang
 
-test_home() # Call tests on Homepage (Index.html)
+test_home() # Call tests on Homepage (Index.html)   ##### REMOVE THIS IF THERE ARE IMPORT ISSUES
